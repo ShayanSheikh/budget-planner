543e1330d08dd12d822fee7fdcd384875fc45aee
Shayan Sheikh <najam.shayan@gmail.com>
2020-05-18 14:47:54 -0400

    Add separate login component

---
+++ src/Login.js
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import { GoogleLogin } from 'react-google-login';
+import { logIn } from './session';
+import { AppContext } from './AppProvider';
+
+const CLIENT_ID = '26226890893-7uke9sajq33a0ddg5896nfkvp7phulmd.apps.googleusercontent.com'
+
+const Login = () => {
+  const { state, setState } = useContext(AppContext);
+
+  const onLoginFailure = () => {
+    alert('Problem with logging in, please try again');
+  };
+
+  const onLoginSuccess = (response) => {
+    logIn(response.accessToken);
+    setState({ ...state, user: response.accessToken });
+  }
+
+  return (
+      <GoogleLogin
+        clientId={CLIENT_ID}
+        buttonText="Login"
+        onSuccess={onLoginSuccess}
+        onFailure={onLoginFailure}
+        cookiePolicy={'single_host_origin'}
+      />
+  );
+}
+
+export default Login;
