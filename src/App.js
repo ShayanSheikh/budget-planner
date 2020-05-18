c4fcc95240bbeb2f53cf1edd46c658142f164801
Shayan Sheikh <najam.shayan@gmail.com>
2020-05-18 17:08:46 -0400

    Use context in App and welcome user

--- src/App.js
+++ src/App.js
@@ -10,13 +10,17 @@ import { AppContext } from './AppProvider';
 const CLIENT_ID = '26226890893-7uke9sajq33a0ddg5896nfkvp7phulmd.apps.googleusercontent.com'
 
 const App = () => {
-  const [expensePercentage, setExpensePercentage] = useState(1);
-  const [salary, setSalary] = useState('');
+  const { state, setState } = useContext(AppContext);
+  const { user, elections } = state;
+
+  const [expensePercentage, setExpensePercentage] = useState(elections.expensePercentage);
+  const [salary, setSalary] = useState(elections.salary);
   const [expenses, setExpenses] = useState('');
   const [savings, setSavings] = useState('');
 
   const onLogoutSuccess = () => {
     logOut();
+    setState({ ...state, user: null })
   }
 
   const expenseText = (value) => {
@@ -39,47 +43,48 @@ const App = () => {
           onLogoutSuccess={onLogoutSuccess}
         />
       </Box>
-        <Container>
-          <Typography id="discrete-slider" gutterBottom>
-            Expenses
-          </Typography>
-          <Slider
-            value={expensePercentage}
-            getAriaValueText={expenseText}
-            onChange={(e, val) => setExpensePercentage(val)}
-            step={0.1}
-            min={1}
-            max={30}
-            valueLabelDisplay="on"
-          />
-          <Grid container spacing={3}>
-            <Grid item xs={6}>
-              Your Salary
-            </Grid>
-            <Grid item xs={6}>
-              <TextField
-                label="Enter Salary"
-                value={salary}
-                onChange={e => setSalary(e.target.value)}
-                InputProps={{
-                  inputComponent: SalaryInput,
-                }}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              Your Expenses
-            </Grid>
-            <Grid item xs={6}>
-              <MoneyDisplay value={expenses}/>
-            </Grid>
-            <Grid item xs={6}>
-              Your Savings
-            </Grid>
-            <Grid item xs={6}>
-              <MoneyDisplay value={savings} />
-            </Grid>
+      <h2>{`Welcome ${user}`}</h2>
+      <Container>
+        <Typography id="discrete-slider" gutterBottom>
+          Expenses
+        </Typography>
+        <Slider
+          value={expensePercentage}
+          getAriaValueText={expenseText}
+          onChange={(e, val) => setExpensePercentage(val)}
+          step={0.1}
+          min={1}
+          max={30}
+          valueLabelDisplay="on"
+        />
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            Your Salary
           </Grid>
-        </Container>
+          <Grid item xs={6}>
+            <TextField
+              label="Enter Salary"
+              value={salary}
+              onChange={e => setSalary(e.target.value)}
+              InputProps={{
+                inputComponent: SalaryInput,
+              }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            Your Expenses
+          </Grid>
+          <Grid item xs={6}>
+            <MoneyDisplay value={expenses}/>
+          </Grid>
+          <Grid item xs={6}>
+            Your Savings
+          </Grid>
+          <Grid item xs={6}>
+            <MoneyDisplay value={savings} />
+          </Grid>
+        </Grid>
+      </Container>
     </Container>
     :
     <Login />
