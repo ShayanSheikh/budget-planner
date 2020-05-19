7688851cc97ef64b79ccb6f719b711e5020ae548
Shayan Sheikh <najam.shayan@gmail.com>
2020-05-19 15:10:57 -0400

    Style login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { GoogleLogin } from 'react-google-login';
 import { logIn } from '../session';
+import { Box, Container } from '@material-ui/core';
 import { AppContext } from '../AppProvider';
 
 const CLIENT_ID = '26226890893-7uke9sajq33a0ddg5896nfkvp7phulmd.apps.googleusercontent.com'
@@ -9,6 +10,7 @@ const Login = () => {
   const { state, setState } = useContext(AppContext);
 
   const onLoginFailure = (error) => {
+    alert('There was a problem with logging you in, please try again');
     console.log(error);
   };
 
@@ -18,13 +20,18 @@ const Login = () => {
   }
 
   return (
-    <GoogleLogin
-      clientId={CLIENT_ID}
-      buttonText="Login"
-      onSuccess={onLoginSuccess}
-      onFailure={onLoginFailure}
-      cookiePolicy={'single_host_origin'}
-    />
+    <Container maxWidth="sm">
+      <Box display="flex" justifyContent="space-between">
+        <h1>Budget Planner</h1>
+        <GoogleLogin
+          clientId={CLIENT_ID}
+          buttonText="Login"
+          onSuccess={onLoginSuccess}
+          onFailure={onLoginFailure}
+          cookiePolicy={'single_host_origin'}
+        />
+      </Box>
+    </Container>
   );
 }
 
