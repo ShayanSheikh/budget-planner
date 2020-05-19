4b5410676fa9da285a00a23ab0c04339cdb0944f
Shayan Sheikh <najam.shayan@gmail.com>
2020-05-19 13:16:00 -0400

    Updates

---
+++ precache-manifest.a4573b243788a1a04d8b2a1555f00eed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4619837551b2c1a087840b78cf9cfe5c",
+    "url": "/budget-planner/index.html"
+  },
+  {
+    "revision": "3e1d16f7ff931f110094",
+    "url": "/budget-planner/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ea48bec037f7da7a08ea",
+    "url": "/budget-planner/static/js/2.38efcaf6.chunk.js"
+  },
+  {
+    "revision": "8a9a2ae4a25f202f52818f8f8074424e",
+    "url": "/budget-planner/static/js/2.38efcaf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e1d16f7ff931f110094",
+    "url": "/budget-planner/static/js/main.6b28fdfe.chunk.js"
+  },
+  {
+    "revision": "94ff00ee44f8bb10befc",
+    "url": "/budget-planner/static/js/runtime-main.f1a687f8.js"
+  }
+]);
